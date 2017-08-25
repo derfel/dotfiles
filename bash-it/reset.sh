023382054f8f9cf37625eacaa99fed71b728e92e
Derfel <code@derfel.net>
2017-08-26 01:24:49 +0200

    * Add alias for thefuck command.

--- bash-it/reset.sh
+++ bash-it/reset.sh
@@ -5,7 +5,7 @@ bash-it disable alias all
 bash-it disable plugin all
 bash-it disable completion all
 
-bash-it enable alias clipboard
+bash-it enable alias clipboard fuck
 bash-it enable plugin alias-completion autojump base dirs hg
 bash-it enable completion awscli bash-it django system tmux
 
